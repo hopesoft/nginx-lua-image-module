8a04b3cfdbccb304a0204741b384ab4732aa228b
hopesoft <hopesoft@126.com>
2014-08-22 01:32:54 +0800

    update table.contains

--- lua/thumbnail.lua
+++ lua/thumbnail.lua
@@ -1,6 +1,6 @@
 -- nginx thumbnail module 
--- last update : 2014/8/3
--- version     : 0.4
+-- last update : 2014/8/21
+-- version     : 0.4.1
 
 local c  = require 'config'
 
@@ -12,11 +12,13 @@ local c  = require 'config'
 	img_width         :缩略图高度
 	img_size          :缩略图宽x高
 	img_crop_type     :缩略图裁剪类型
+	cur_uri_reg_model :缩略图uri正则规则
 ]]
-local uri = ngx.var.uri;
-local ngx_img_root = ngx.var.image_root;
-local ngx_thumbnail_root = ngx.var.thumbnail_root;
-local img_width,img_height,img_size,img_crop_type = 0;
+local uri = ngx.var.uri
+local ngx_img_root = ngx.var.image_root
+local ngx_thumbnail_root = ngx.var.thumbnail_root
+local img_width,img_height,img_size,img_crop_type = 0
+local cur_uri_reg = c.default_uri_reg
 
 --[[
 	日志函数
@@ -33,44 +35,50 @@ end
 
 --	匹配链接对应缩略图规则
 function table.contains(table,element)
+	img_crop_type = 0		
     for _, value in pairs(c.cfg) do
-        local dir = value['dir'];
-        local sizes = value['sizes'];
-        _,_,img_width,img_height = string.find(uri,''..dir..'+.*_([0-9]+)x([0-9]+)');
-        if(img_width and img_height ) then
-            img_size = img_width..'x'..img_height;
+        local dir = value['dir']
+        local sizes = value['sizes']
+		local uri_reg = value['uri_reg']
+        _,_,img_width,img_height = string.find(uri,''..dir..'+.*_([0-9]+)x([0-9]+)')
+        if(img_width and img_height and img_crop_type==0) then
+            img_size = img_width..'x'..img_height
             for _, value in pairs(sizes) do
+				if(uri_reg) then
+					lua_log('value[uri_reg]==='..uri_reg)
+				else
+					lua_log('value[uri_reg]===nil,dir='..dir..',cur_uri_reg='..cur_uri_reg)
+				end
+				cur_uri_reg = uri_reg or cur_uri_reg			
                 if (img_size == value) then
-                    img_crop_type=1;
-                    return true;
+                    img_crop_type=1
+                    return true
                 elseif (img_size..'_' == value) then
-                    img_crop_type=2;
-                    return true;					
+                    img_crop_type=2
+                    return true			
                 elseif (img_size..'!' == value) then
-                    img_crop_type=3;
-                    return true;
+                    img_crop_type=3
+                    return true
                 elseif (img_size..'^' == value) then
-                    img_crop_type=4;
-                    return true;
+                    img_crop_type=4
+                    return true
                 elseif (img_size..'>' == value) then
-                    img_crop_type=5;
-                    return true;
+                    img_crop_type=5
+                    return true
                 elseif (img_size..'$' == value) then
-                    img_crop_type=6;
-                    img_size = img_width..'x';
-                    return true;					
+                    img_crop_type=6
+                    img_size = img_width..'x'
+                    return true	
                 end
-            end
-        end
+            end	
+        end			
     end
     return false
 end
 
 -- 拼接gm命令
 local function generate_gm_command(img_crop_type,img_original_path,img_size,img_thumbnail_path)
-	local cmd
-	cmd = c.gm_path .. ' convert ' .. img_original_path
-	
+	local cmd = c.gm_path .. ' convert ' .. img_original_path
 	if (img_crop_type == 1) then
 		cmd = cmd .. ' -thumbnail '  .. img_size .. ' -background ' .. c.img_background_color .. ' -gravity center -extent ' .. img_size
 	elseif (img_crop_type == 2) then
@@ -82,55 +90,50 @@ local function generate_gm_command(img_crop_type,img_original_path,img_size,img_
 	elseif (img_crop_type == 5 or img_crop_type == 6) then
 		cmd = cmd .. ' -resize "'  .. img_size .. '>"'
 	else
-		lua_log('img_crop_type error:'..img_crop_type,ngx.ERR);
-		ngx.exit(404);
+		lua_log('img_crop_type error:'..img_crop_type,ngx.ERR)
+		ngx.exit(404)
 	end	
 	cmd = cmd .. ' ' .. img_thumbnail_path
 	return cmd
 end
 
 lua_log("ngx_thumbnail_root======="..ngx_thumbnail_root)
-
---[[
-	1.判断原图是否存在，如原图不存在则进入下一步；如存在则跳至4
-	2.判断是否显示默认图片，如不显示则404退出；如显示则进入下一步
-	3.判断是否存在默认图片，如不存在则404退出；如存在则将默认图片代替原始图片，进入下一步；	
-]]
-local img_original_uri = string.gsub(uri, '_[0-9]+x[0-9]+','');
-local img_exist=io.open(ngx_img_root .. img_original_uri);
-if not img_exist then
-    if not c.enabled_default_img then
-        lua_log(img_original_uri..' is not exist!',ngx.ERR);
-        ngx.exit(404);
-    else
-        img_exist=io.open(ngx_img_root ..  c.default_img_uri);
-        if img_exist then
-            lua_log(img_original_uri .. 'is not exist! crop image with default image');
-            img_original_uri = c.default_img_uri;
-        else
-            lua_log(img_original_uri..' is not exist!',ngx.ERR);
-            ngx.exit(404);
-        end
-    end;
-end;
-
---[[
-	4.判断缩略图链接与规则是否匹配，如不匹配，则404退出；如匹配跳至5
-	5.拼接graphicsmagick，并执行
-]]
+	
 if not table.contains(c.cfg, uri) then
-    lua_log(uri..' is not match!',ngx.ERR);
-    ngx.exit(404);
+    lua_log(uri..' is not match!',ngx.ERR)
+    ngx.exit(404)
 else
+    lua_log(uri..' is match!')
+	local img_original_uri = string.gsub(uri, cur_uri_reg, '')
+	lua_log('img_original_uri_old===' .. uri)
+	lua_log('cur_uri_reg===' .. cur_uri_reg)	
+	lua_log('img_original_uri_new===' .. img_original_uri)
+	local img_exist=io.open(ngx_img_root .. img_original_uri)
+	if not img_exist then
+		if not c.enabled_default_img then
+			lua_log(img_original_uri..' is not exist!',ngx.ERR)
+			ngx.exit(404)
+		else
+			img_exist=io.open(ngx_img_root ..  c.default_img_uri)
+			if img_exist then
+				lua_log(img_original_uri .. ' is not exist! crop image with default image')
+				img_original_uri = c.default_img_uri
+			else
+				lua_log(img_original_uri..' is not exist!',ngx.ERR)
+				ngx.exit(404)
+			end
+		end
+	end
+	
     local img_original_path  = ngx_img_root .. img_original_uri
     local img_thumbnail_path = ngx_thumbnail_root .. uri
     local gm_command         = generate_gm_command(img_crop_type,img_original_path,img_size,img_thumbnail_path)
 	
     if (gm_command) then
-		lua_log('gm_command======'..gm_command);
+		lua_log('gm_command======'..gm_command)
         _,_,img_thumbnail_dir,img__thumbnail_filename=string.find(img_thumbnail_path,'(.-)([^/]*)$')
-        os.execute('mkdir -p '..img_thumbnail_dir);
-        os.execute(gm_command);
+        os.execute('mkdir -p '..img_thumbnail_dir)
+        os.execute(gm_command)
     end
-    ngx.req.set_uri('/thumbnail'..uri);
+    ngx.req.set_uri('/thumbnail'..uri)
 end
